f56eadc23a41fd90641d1bb5bf0d38e047dc6548
sadegh <khanzadisadegh@gmail.com>
2023-12-14 21:53:13 +0330

    USER_STORY: TASK_NUMBER: BUG_NUMBER: COMMENT: --COMMIT_TYPE: MERGE NEWS --FIX_BUG: 0.0.0
    2023/Dec/14 - 9:50 PM

---
+++ webMessenger/src/main/java/com/khanzadi/webMessenger/WebMessengerApplication.java
@@ -0,0 +1,13 @@
+package com.khanzadi.webMessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMessengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMessengerApplication.class, args);
+	}
+
+}
---
+++ webMessenger/src/test/java/com/khanzadi/webMessenger/WebMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khanzadi.webMessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
